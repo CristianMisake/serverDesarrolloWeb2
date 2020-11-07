b1aabfc5c82aacef684d2608d03678278eef8e84
Cristian Grondona <cristiangrondon@gmail.com>
2020-11-07 17:09:41 -0500

    primeros cambios

---
+++ app/migrations/20201107220231-create-usuarios.js
@@ -0,0 +1,37 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('usuarios', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      user: {
+        type: Sequelize.STRING
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      perfil: {
+        type: Sequelize.INTEGER
+      },
+      estado: {
+        type: Sequelize.INTEGER,
+        defaultValue: 1,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('usuarios');
+  }
+};
